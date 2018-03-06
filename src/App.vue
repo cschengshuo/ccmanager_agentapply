d49acd23e0e18b36c0f4b3bb7fafe52ea809c958
chengshuo <chengshuo@winsyo.com>
2018-03-06 11:43:34 +0800

    完成页面功能

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,174 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+  <div>
+    <form novalidate class="md-layout" @submit.prevent="validateUser">
+      <md-card class="md-layout-item md-size-50 md-small-size-100">
+        <md-card-header>
+          <div class="md-title">代理申请</div>
+        </md-card-header>
+
+        <md-card-content>
+          <md-field :class="getValidationClass('name')">
+            <label for="name">姓名</label>
+            <md-input name="name" id="name" v-model="form.name" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.name.required">请输入姓名</span>
+            <span class="md-error" v-else-if="!$v.form.name.maxlength">姓名超过最大长度</span>
+          </md-field>
+
+          <md-field :class="getValidationClass('company')">
+            <label for="company">公司（选填）</label>
+            <md-input id="company" name="company" v-model="form.company" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.company.maxlength">公司名超过最大长度</span>
+          </md-field>
+
+          <md-field :class="getValidationClass('phone')">
+            <label for="phone">手机</label>
+            <md-input type="phone" name="phone" id="phone" v-model="form.phone" :disabled="sending" />
+            <span class="md-error" v-if="!$v.form.phone.required">请输入手机号</span>
+            <span class="md-error" v-else-if="!$v.form.phone.isPhoneNumber">请输入正确的手机号</span>
+          </md-field>
+
+          <md-field :class="getValidationClass('captcha')">
+            <label for="captcha">验证码</label>
+            <md-input name="captcha" id="captcha" v-model="form.captcha" :disabled="sending" />
+            <md-button class="md-primary" @click="sendCaptcha" :disabled="canSendCaptcha">{{ text }}</md-button>
+            <span class="md-error" v-if="!$v.form.captcha.required">请输入手机验证码</span>
+            <span class="md-error" v-else-if="!$v.form.captcha.numeric">手机验证码内容应为数字</span>
+            <span class="md-error" v-else-if="!$v.form.captcha.minlength">手机验证码应为6位数字</span>
+            <span class="md-error" v-else-if="!$v.form.captcha.maxlength">手机验证码应为6位数字</span>
+          </md-field>
+        </md-card-content>
+
+        <md-progress-bar md-mode="indeterminate" v-if="sending" />
+
+        <md-card-actions>
+          <md-button type="submit" class="md-primary md-raised" :disabled="sending">点击提交</md-button>
+        </md-card-actions>
+      </md-card>
+
+      <md-snackbar :md-active.sync="userSaved">用户 {{ form.name }} 的代理申请提交成功</md-snackbar>
+      <md-snackbar :md-active.sync="showSendCaptcha">验证码发送成功</md-snackbar>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import {
+  required,
+  minLength,
+  maxLength,
+  numeric
+} from 'vuelidate/lib/validators'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'FormValidation',
+  data: () => ({
+    form: {
+      name: null,
+      company: null,
+      phone: null,
+      captcha: null
+    },
+    userSaved: false,
+    sending: false,
+    showSendCaptcha: false,
+    time: 0,
+    sendedCaptcha: false
+  }),
+  validations: {
+    form: {
+      name: {
+        required,
+        maxLength: maxLength(15)
+      },
+      company: {
+        maxLength: maxLength(35)
+      },
+      phone: {
+        required,
+        isPhoneNumber (value) {
+          const reg = /^(13[0-9]|14[579]|15[0-3,5-9]|16[6]|17[0135678]|18[0-9]|19[89])\d{8}$/
+          return reg.test(value)
+        }
+      },
+      captcha: {
+        required,
+        numeric,
+        minLength: minLength(6),
+        maxLength: maxLength(6)
+      }
+    }
+  },
+  methods: {
+    getValidationClass (fieldName) {
+      const field = this.$v.form[fieldName]
+
+      if (field) {
+        return {
+          'md-invalid': field.$invalid && field.$dirty
+        }
+      }
+    },
+    clearForm () {
+      this.$v.$reset()
+      this.form.name = null
+      this.form.company = null
+      this.form.phone = null
+      this.form.captcha = null
+    },
+    sendCaptcha () {
+      this.showSendCaptcha = true
+      this.time = 60
+      this.timer()
+    },
+    timer () {
+      if (this.time > 0) {
+        this.time--
+        setTimeout(this.timer, 1000)
+      }
+    },
+    saveUser () {
+      this.sending = true
+
+      // TODO Instead of this timeout, here you can call your API
+      window.setTimeout(() => {
+        this.userSaved = true
+        this.sending = false
+        this.clearForm()
+      }, 1500)
+    },
+    validateUser () {
+      this.$v.$touch()
+
+      if (!this.$v.$invalid) {
+        this.saveUser()
+      }
+    }
+  },
+  computed: {
+    text: function () {
+      return this.time > 0 ? this.time + 's 后重新获取' : '获取验证码'
+    },
+    canSendCaptcha: function () {
+      return this.time > 0
+    }
   }
 }
 </script>
 
+<style lang="scss" scoped>
+.md-progress-bar {
+  position: absolute;
+  top: 0;
+  right: 0;
+  left: 0;
+}
+</style>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueMaterial from 'vue-material'
+import Vuelidate from 'vuelidate'
 import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css' // This line here
 import App from './App'
 
+Vue.use(Vuelidate)
 Vue.use(VueMaterial)
 
 Vue.config.productionTip = false
